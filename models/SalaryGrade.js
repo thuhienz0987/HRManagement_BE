d432b546daa1429976556bbddb3ebac77fd828ef
2122059 <21522059@gm.uit.edu.vn>
2023-09-28 16:48:08 +0700

    salary grade

---
+++ models/SalaryGrade.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const salaryGradeSchema = mongoose.Schema({
+    code:{
+        type: String,
+        required: [true, 'A salary grade must have a code'],
+        minLength: [1,'A code of salary grade must have minimum of 1 character'],
+        maxLength:[4,'A code of salary grade must have maximum of 4 character'],
+        unique: [true,'A code of salary grade with the same name has already exists'],
+    },
+    factor:{
+        type: Number,
+        required: [true,'A salary grade must have a factor'],
+    },
+    positionCode:{
+        type: mongoose.Types.ObjectId,
+        ref:'Position',
+        required: [true,'A salary grade must link position'],
+    },
+    isDeleted:{
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+
+})
+
+const SalaryGrade = mongoose.model('SalaryGrade', salaryGradeSchema)
+export default SalaryGrade
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ import refreshRouters from './refreshRouters.js';
 import userRouters from './userRouters.js';
 import positionRouters from './positionRouters.js';
 import allowanceRouter from "./allowanceRouters.js";
+import bonusRouter from "./bonusRouters.js";
+import salaryGradeRouter from "./salaryGradeRouters.js"
 
 const router = Router();
 
@@ -14,6 +16,8 @@ router.use(refreshRouters);
 
 router.use(positionRouters);
 router.use(allowanceRouter);
+router.use(bonusRouter);
+router.use(salaryGradeRouter);
 
 router.use(verifyJWT);
 router.get('/test', (req, res) => {res.status(200).json('OK')});
