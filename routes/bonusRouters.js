4c62097d49cd3134a822eb478bda332d617baa3e
PhuocTri0106 <you@PhuocTri0106.com>
2023-09-29 16:18:40 +0700

    update Authorization

--- routes/bonusRouters.js
+++ routes/bonusRouters.js
@@ -1,11 +1,14 @@
 import Router from "express";
 import { deleteBonus, getBonus, getBonuses, postBonus, updateBonus } from "../controllers/bonusController.js";
+import verifyRoles from "../middlewares/verifyRoles.js";
+import ROLES_LIST from "../config/roles_list.js";
+
 const bonusRouter = Router();
 
-bonusRouter.get('/bonuses',getBonuses);
+bonusRouter.get('/bonuses',verifyRoles(ROLES_LIST.HRManager,ROLES_LIST.Admin),getBonuses);
 bonusRouter.get('/bonus/:id',getBonus);
-bonusRouter.post('/bonus',postBonus);
-bonusRouter.put('/bonus/:id',updateBonus);
-bonusRouter.delete('/bonus/:id',deleteBonus);
+bonusRouter.post('/bonus',verifyRoles(ROLES_LIST.Admin),postBonus);
+bonusRouter.put('/bonus/:id',verifyRoles(ROLES_LIST.Admin),updateBonus);
+bonusRouter.delete('/bonus/:id',verifyRoles(ROLES_LIST.Admin),deleteBonus);
 
 export default bonusRouter;
--- routes/salaryGradeRouters.js
+++ routes/salaryGradeRouters.js
@@ -1,12 +1,14 @@
 import Router from "express";
 import { deleteSalaryGrade, getSalaryGrade, getSalaryGrades, postSalaryGrade, updateSalaryGrade } from "../controllers/salaryGradeController.js";
+import verifyRoles from "../middlewares/verifyRoles.js";
+import ROLES_LIST from "../config/roles_list.js";
 
 const salaryGradeRouter = Router()
 
-salaryGradeRouter.get('/salaryGrades',getSalaryGrades)
+salaryGradeRouter.get('/salaryGrades',verifyRoles(ROLES_LIST.HRManager,ROLES_LIST.Admin),getSalaryGrades)
 salaryGradeRouter.get('/salaryGrade/:id',getSalaryGrade)
-salaryGradeRouter.post('/salaryGrade',postSalaryGrade)
-salaryGradeRouter.put('/salaryGrade/:id',updateSalaryGrade)
-salaryGradeRouter.delete('/salaryGrade/:id',deleteSalaryGrade)
+salaryGradeRouter.post('/salaryGrade',verifyRoles(ROLES_LIST.Admin),postSalaryGrade)
+salaryGradeRouter.put('/salaryGrade/:id',verifyRoles(ROLES_LIST.Admin),updateSalaryGrade)
+salaryGradeRouter.delete('/salaryGrade/:id',verifyRoles(ROLES_LIST.Admin),deleteSalaryGrade)
 
 export default salaryGradeRouter
