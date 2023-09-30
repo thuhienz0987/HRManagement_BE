8e0f58e647d5018b5021b29a50ddce662dcd6d7e
2122059 <21522059@gm.uit.edu.vn>
2023-09-30 11:39:54 +0700

    update server

---
+++ models/Salary.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose'
+
+const salarySchema = mongoose.Schema({
+    idUser:{
+        type:  mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true,'A salary must link a id of user']
+    },
+    idPosition:{
+        type:  mongoose.Types.ObjectId,
+        ref:'Position',
+        required:[true,'A salary must link a id of position']
+    },
+    idSalaryGrade:{
+        type:  mongoose.Types.ObjectId,
+        ref:'SalaryGrade',
+        required:[true,'A salary must link a id of salary grade']
+    },
+    idBonus:{
+        type:  mongoose.Types.ObjectId,
+        ref:'Bonus',
+        required:[true,'A salary must link a id of bonus']
+    },
+    idAllowance:{
+        type:  mongoose.Types.ObjectId,
+        ref:'Allowance',
+        required:[true,'A salary must link a id of allowance']
+    },
+    incomeTax:{
+        type: Number,
+        required: true,
+        default: 8,
+        min: 0,
+        max: 100,
+    },
+    payDay:{
+        type: Date,
+        default: Date.now(),
+        required: true,
+    },
+    daysOff:{
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    totalSalary:{
+        type: Number,
+        default: 0,
+        required: true
+    }
+    
+})
+
+const Salary = mongoose.model('Salary', salarySchema)
+export default Salary
--- models/User.js
+++ models/User.js
@@ -130,6 +130,6 @@ userSchema.pre('save', async function(next) {
     next();
 })
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
