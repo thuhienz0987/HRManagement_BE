3cb736c82e01af9644afd1a683b5da185d4348cb
PhuocTri0106 <you@PhuocTri0106.com>
2023-11-17 14:57:29 +0700

    fix team controller and cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
 const allowedOrigins = [
-    // 'https://fashion-app-0m3s.onrender.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost:3000/favicon.ico'
+  // 'https://fashion-app-0m3s.onrender.com',
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3000/favicon.ico",
 ];
 
 export default allowedOrigins;
