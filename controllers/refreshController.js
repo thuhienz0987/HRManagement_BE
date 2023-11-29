be971ad706c5c3592a48e745bca4c2900bac8f5a
PhuocTri0106 <you@PhuocTri0106.com>
2023-11-29 07:54:13 +0700

    fix bug

--- controllers/refreshController.js
+++ controllers/refreshController.js
@@ -1,44 +1,41 @@
-import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
-import UnauthorizedError from '../errors/unauthorizedError.js';
-import ForbiddenError  from '../errors/forbiddenError.js';
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import ForbiddenError from "../errors/forbiddenError.js";
 
 const maxAgeAccessToken = 60 * 15;
 
 const handleRefreshToken = async (req, res) => {
-    console.log(req.cookies);
-    const cookies = req.cookies;
+  // console.log(req.cookies);
+  // const cookies = req.cookies;
+  const { refreshToken } = req.body;
 
-    // check if there are cookies --> if yes then check if jwt exists
-    if(!cookies?.jwt) throw new UnauthorizedError("No refresh token found");;
-    refreshToken = cookies.jwt;
-    
-    // find the user that owned this jwt
-    const user = await User.findOne({ refreshToken }).exec();
-    console.log(user);
-    if(!user) throw new ForbiddenError("invalid refresh token"); // token does not match with any user
-    
-    // evaluate jwt
-    jwt.verify(
-        refreshToken, 
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
-            console.log(decoded);
-            if (err || user.id.toString() !== decoded.userId) 
-                throw new ForbiddenError("invalid refresh token");;
+  // check if there are cookies --> if yes then check if jwt exists
+  // if(!cookies?.jwt) throw new UnauthorizedError("No refresh token found");;
+  // refreshToken = cookies.jwt;
 
-            const accessToken = jwt.sign(
-                {
-                    "userInfo": {
-                        "userId": user.id,
-                    }
-                },
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: maxAgeAccessToken }
-            );
-            res.json({ accessToken });
-        }
+  // find the user that owned this jwt
+  const user = await User.findOne({ refreshToken }).exec();
+  console.log(user);
+  if (!user) throw new ForbiddenError("invalid refresh token"); // token does not match with any user
+
+  // evaluate jwt
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    console.log(decoded);
+    if (err || user._id.toString() !== decoded.userId)
+      throw new ForbiddenError("invalid refresh token");
+
+    const accessToken = jwt.sign(
+      {
+        userInfo: {
+          userId: user._id,
+          roles: user.roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: maxAgeAccessToken }
     );
+    res.json({ accessToken });
+  });
 };
 
 export default handleRefreshToken;
