5d0dbf17bf8439592d4bb9db76ed83dc1db2d9be
PhuocTri0106 <you@PhuocTri0106.com>
2023-12-18 09:37:45 +0700

    fix get salary

--- routes/salaryRouters.js
+++ routes/salaryRouters.js
@@ -1,14 +1,21 @@
 import Router from "express";
-import { confirmSalary, getSalaries, getSalary, postSalary, updateSalary } from "../controllers/salaryController.js";
+import {
+  confirmSalary,
+  getSalaries,
+  getSalary,
+  getSalaryByUserId,
+  postSalary,
+  updateSalary,
+} from "../controllers/salaryController.js";
 
-const salaryRouter = Router()
+const salaryRouter = Router();
 
-salaryRouter.get('/salaries', getSalaries);
-salaryRouter.get('/salary/:id', getSalary);
-salaryRouter.get('/salaryByUserId/:id', getSalary);
+salaryRouter.get("/salaries", getSalaries);
+salaryRouter.get("/salary/:id", getSalary);
+salaryRouter.get("/salaryByUserId/:id", getSalaryByUserId);
 
-salaryRouter.post('/salary',postSalary);
-salaryRouter.put('/salary/:id',updateSalary);
-salaryRouter.put('/confirmSalary/:id',confirmSalary);
+salaryRouter.post("/salary", postSalary);
+salaryRouter.put("/salary/:id", updateSalary);
+salaryRouter.put("/confirmSalary/:id", confirmSalary);
 
 export default salaryRouter;
