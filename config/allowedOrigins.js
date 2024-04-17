272f975889437f923bae7a7fc168ffa7776ea1dc
KhoiMaiDinh <100573122+KhoiMaiDinh@users.noreply.github.com>
2024-04-17 23:58:53 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,13 @@
 const allowedOrigins = [
   // 'https://fashion-app-0m3s.onrender.com',
+  "https://hr-management-fe-rf3w-dqo9zfngh-mai-dinh-khois-projects.vercel.app",
+  "https://hr-management-fe-rf3w.vercel.app",
+  "https://hr-management-fe-rf3w.vercel.app/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3000/favicon.ico",
-  "https://hr-management-fe-rf3w-dqo9zfngh-mai-dinh-khois-projects.vercel.app",
-  "https://hr-management-fe-rf3w.vercel.app",
 ];
 
 export default allowedOrigins;
