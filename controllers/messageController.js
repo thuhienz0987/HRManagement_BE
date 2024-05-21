d215fc8beb76df945743c8eee0623b5bfca80e7c
PhuocTri0106 <you@PhuocTri0106.com>
2024-05-22 00:22:17 +0700

    get message history

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,4 +1,7 @@
 import Message from "../models/Message.js";
+import GroupMessage from "../models/GroupMessage.js";
+import GroupChat from "../models/GroupChat.js";
+import GroupMember from "../models/GroupMember.js";
 
 const saveMessage = async (req, res) => {
   try {
@@ -30,6 +33,80 @@ const updateMessage = async (req, res) => {
   }
 };
 
+const getUserMessageHistory = async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    // Find distinct users who exchanged messages with the given user
+    const distinctUsers = await Message.find({
+      $or: [{ senderId: userId }, { receiverId: userId }],
+    })
+      .distinct("receiverId")
+      .populate("receiverId")
+      .populate("senderId");
+    // Get the last message exchanged with each distinct user
+    const messageHistory = await Promise.all(
+      distinctUsers.map(async (otherUserId) => {
+        const lastMessage = await Message.findOne({
+          $or: [
+            { senderId: userId, receiverId: otherUserId },
+            { senderId: otherUserId, receiverId: userId },
+          ],
+        })
+          .sort({ createdAt: -1 })
+          .limit(1); // Get the last message exchanged
+        return {
+          userId: otherUserId._id,
+          userName: otherUserId.name,
+          avatar: otherUserId.avatarImage,
+          lastMessage: lastMessage
+            ? lastMessage.message
+            : "No message available",
+        };
+      })
+    );
+
+    const userCreatedGroups = await GroupChat.find({
+      creatorId: userId,
+    }).populate("creatorId");
+
+    const userMemberGroups = await GroupMember.find({
+      memberId: userId,
+    })
+      .populate("groupId")
+      .populate("memberId");
+
+    const allGroups = [
+      ...userCreatedGroups,
+      ...userMemberGroups.map((group) => group.groupId),
+    ];
+
+    const groupHistory = await Promise.all(
+      allGroups.map(async (group) => {
+        const lastMessage = await GroupMessage.findOne({ groupId: group._id })
+          .sort({ createdAt: -1 })
+          .limit(1);
+
+        return {
+          groupId: group._id,
+          groupName: group.name,
+          groupImage: group.groupImage,
+          lastMessage: lastMessage
+            ? lastMessage.message
+            : "No message available", // Get the last message content, or 'No message available' if no message
+        };
+      })
+    );
+    res
+      .status(200)
+      .json({
+        result: { messageHistory: messageHistory, groupHistory: groupHistory },
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message }); // Handle any errors
+  }
+};
+
 const deleteMessage = async (req, res) => {
   try {
     var deleteMessage = await Message.deleteOne({ _id: req.params._id });
@@ -41,4 +118,4 @@ const deleteMessage = async (req, res) => {
   }
 };
 
-export { deleteMessage, saveMessage, updateMessage };
+export { deleteMessage, saveMessage, getUserMessageHistory, updateMessage };
